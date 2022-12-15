592a2beca1e4a63fd5e78a67ed41e289b94af27e
osamu-kj <kevin.jerebica@gmail.com>
2022-12-15 10:33:17 -0500

    General: add general API route for fetching key messages

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,25 @@
 const express = require("express")
+const body_parser = require("body-parser")
+const base = require("base-64")
+
+
 const app = express()
 const PORT = 443 
 
+// express configuration 
+app.use(body_parser.urlencoded({
+    extended: true
+}))
 
-app.get("/", (req, res) => {
-    res.send("hello world\n");
+// Backend - Routes
+
+// API - Routes 
+app.post("/client-fetch-keys", (req, res) => { // client sends here the keys
+    let keys = req.body.keys
+    console.log(base.decode(keys));
+
+
+    res.send("PIVOTER_OK\n");
 })
 
 app.listen(PORT)
